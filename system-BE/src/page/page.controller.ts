6cdd049c8b50805081b6cc4bcd8577aaca7b21a7
sasa-bonta <bonta2000@mail.ru>
2021-12-28 17:22:22 +0200

    system-BE page

--- system-BE/src/page/page.controller.ts
+++ system-BE/src/page/page.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PageService } from './page.service';
+import { pageListStub } from './stubs/pageStubs';
 
 @Controller('page')
-export class PageController {}
+export class PageController {
+  constructor(protected pageService: PageService) {}
+
+  @Get()
+  getItemsPerPage(@Query('link') link) {
+    return this.pageService.getItemsOnPage(link);
+  }
+
+  @Get('test')
+  getItemsPerPageStubs() {
+    return pageListStub;
+  }
+}
--- system-BE/src/page/page.service.ts
+++ system-BE/src/page/page.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class PageService {}
+export class PageService {
+  async getItemsOnPage(link: string) {
+    return (await axios.get(`http://localhost:5000/page?link=${link}`)).data;
+  }
+}

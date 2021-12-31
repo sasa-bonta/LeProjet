6e685dbbc708f83d75b68eb23c81d3238dc24e51
sasa-bonta <bonta2000@mail.ru>
2021-12-31 12:07:38 +0200

    Reading categories : {category, subCategories {title, items}}

--- parser-enter/src/categories/utils/parseCategoriesFromHtml.ts
+++ parser-enter/src/categories/utils/parseCategoriesFromHtml.ts
@@ -32,19 +32,30 @@ import cheerio from 'cheerio';
 export default (html) => {
   const $ = cheerio.load(html);
   const result = [];
+
   $('ul.first-level > li.first-level').each(function (i, categoryItem) {
     const subCategories = [];
 
     $('div.megameu.uk-margin-top-remove')
       .eq(i)
-      .find('ul.uk-subnav > li.second-level > a')
-      .each(function () {
-        subCategories.push($(this).text().trim().replace(/\n/g, ''));
+      .find('ul.uk-subnav')
+      .each(function (i2, subCategoryItem) {
+        const items = [];
+
+        $(subCategoryItem)
+          .find('li.second-sublevel > a')
+          .each(function () {
+            items.push($(this).text().trim());
+          });
+
+        subCategories.push({
+          title: $(subCategoryItem).find('li.second-level > a').text().trim(),
+          items: items,
+        });
       });
 
     result.push({
       category: $(categoryItem).find('a').text().trim(),
-      index: i,
       subCategories: subCategories,
     });
   });

f970794432615233c55c122050fb038458dc52fa
sasa-bonta <abonta@pentalog.com>
2022-02-13 00:22:44 +0200

    Bugfix: parser-enter: categories

--- parser-enter/src/categories/utils/parseCategoriesFromHtml.ts
+++ parser-enter/src/categories/utils/parseCategoriesFromHtml.ts
@@ -7,31 +7,31 @@ export default (html) => {
   $('ul.first-level > li.first-level').each(function (i, categoryItem) {
     const subCategories = [];
 
-    $('div.tm-megamenu-level2')
+    $('li.second-level')
       .eq(i)
-      .find('ul.uk-subnav')
+      .find('ul')
       .each(function (i2, subCategoryItem) {
         const items = [];
 
         $(subCategoryItem)
-          .find('li.second-sublevel > a')
+          .find('a')
           .each(function () {
             items.push({
               item: $(this).text().trim(),
-              url: $(this).attr('href'),
+              url: $(this).attr('href').replace('https://enter.online', ''),
             });
           });
 
         subCategories.push({
           title: $(subCategoryItem).find('li.second-level > a').text().trim(),
-          url: $(subCategoryItem).find('li.second-level > a').attr('href'),
+          url: $(subCategoryItem).find('li.second-level > a').attr('href')?.replace('https://enter.online', ''),
           items: items,
         });
       });
 
     result.push({
       category: $(categoryItem).find('a').text().trim(),
-      url: $(categoryItem).find('a').attr('href'),
+      url: $(categoryItem).find('a').attr('href').replace('https://enter.online', ''),
       sub_categories: subCategories,
     });
   });

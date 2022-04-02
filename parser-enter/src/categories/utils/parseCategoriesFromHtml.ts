dbd35750d8a779968ba595fc96e855aa4b3cc7f6
sasa-bonta <abonta@pentalog.com>
2022-04-02 11:44:55 +0300

    parser-enter categories work

--- parser-enter/src/categories/utils/parseCategoriesFromHtml.ts
+++ parser-enter/src/categories/utils/parseCategoriesFromHtml.ts
@@ -7,14 +7,14 @@ export default (html) => {
   $('ul.first-level > li.first-level').each(function (i, categoryItem) {
     const subCategories = [];
 
-    $('li.second-level')
+    $('ul.uk-nav')
       .eq(i)
-      .find('ul')
+      .find('li')
       .each(function (i2, subCategoryItem) {
         const items = [];
 
         $(subCategoryItem)
-          .find('a')
+          .find('div.uk-nav > a')
           .each(function () {
             items.push({
               item: $(this).text().trim(),
@@ -23,15 +23,15 @@ export default (html) => {
           });
 
         subCategories.push({
-          title: $(subCategoryItem).find('li.second-level > a').text().trim(),
-          url: $(subCategoryItem).find('li.second-level > a').attr('href')?.substr(20),
+          title: $(subCategoryItem).find('a.level2').text().trim(),
+          url: $(subCategoryItem).find('a.level2').attr('href')?.substr(20),
           items: items,
         });
       });
 
     result.push({
-      category: $(categoryItem).find('a').text().trim(),
-      url: $(categoryItem).find('a').attr('href').substr(20),
+      category: $(categoryItem).find('a.fa-chevron-right').text().trim(),
+      url: $(categoryItem).find('a.fa-chevron-right').attr('href').substr(20),
       sub_categories: subCategories,
     });
   });

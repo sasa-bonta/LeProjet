7f2c8c95c968b41279c207272edacae95f7fc557
sasa-bonta <abonta@pentalog.com>
2022-04-03 18:38:48 +0300

    999 price

--- parser-999/src/price/utils/parseItemsFromHtml.ts
+++ parser-999/src/price/utils/parseItemsFromHtml.ts
@@ -2,5 +2,7 @@ import cheerio from "cheerio";
 
 export default (html) => {
     const $ = cheerio.load(html);
-    return $("span.adPage__content__price-feature__prices__price__value").last().text().trim().replace(/\s/g, '');
+    return $("ul.adPage__content__price-feature__prices").first()
+        .find('span.adPage__content__price-feature__prices__price__value')
+        .first().text().trim().replace(/\s/g, '');
 };

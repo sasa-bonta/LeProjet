56df01b3815ab4edf9ee5a58fc2021be00ffb232
sasa-bonta <abonta@pentalog.com>
2022-04-04 22:31:17 +0300

    category page stubs

--- system-BE/src/page/stubs/pageStubs.ts
+++ system-BE/src/page/stubs/pageStubs.ts
@@ -2,24 +2,20 @@
 // https://enter.online/ru/audiotehnika/naushniki/
 
 export const pageListStub = [
-  {
-    name: 'Apple iPhone 13 Pro',
-    image:
-      'http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^34^_^enter-apple-iphone-13-pro.png',
-  },
-  {
-    name: 'Apple iPhone 13 Pro Max',
-    image:
-      'http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^02^_^enter-apple-iphone-13-pro-max-078.png',
-  },
-  {
-    name: 'Apple iPhone 13 mini',
-    image:
-      'http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^90^_^enter-apple-iphone-13-mini.png',
-  },
-  {
-    name: 'Apple iPhone 13',
-    image:
-      'http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^58^_^enter-apple-iphone-13.png',
-  },
+    {
+        name: "Apple iPhone 13",
+        image: "http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^01^_^enter-apple-iphone-13-09.png"
+    },
+    {
+        name: "Apple iPhone 13 Pro Max",
+        image: "http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^02^_^enter-apple-iphone-13-pro-max-078.png"
+    },
+    {
+        name: "Apple iPhone 13 Pro",
+        image: "http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^34^_^enter-apple-iphone-13-pro.png"
+    },
+    {
+        name: "Apple iPhone 13 mini",
+        image: "http://localhost:3000/public/stubs/enter.online^_^images^_^product^_^thumbs^_^2022^_^90^_^enter-apple-iphone-13-mini.png"
+    }
 ];

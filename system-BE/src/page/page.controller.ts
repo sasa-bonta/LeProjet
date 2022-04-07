2bb4a562e858c746e2701fac6e5faf9fe63668c7
sasa-bonta <abonta@pentalog.com>
2022-04-07 22:20:58 +0300

    load products from category delay 1000

--- system-BE/src/page/page.controller.ts
+++ system-BE/src/page/page.controller.ts
@@ -6,17 +6,20 @@ import { goServer } from '../constants/imageProcessorUrl';
 
 @Controller('page')
 export class PageController {
-  constructor(protected pageService: PageService) {}
+  constructor(protected pageService: PageService) {
+  }
+
+  delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
   @Get()
   getItemsPerPage(@Query('link') link, @Query('page') page) {
     return this.pageService.getItemsOnPage(link, page).then(function (items) {
       return axios
-        .post(
-          goServer,
-          items.map((el) => el.image),
-        )
-        .then(function (r) {
+          .post(
+              goServer,
+              items.map((el) => el.image),
+          )
+          .then(function (r) {
           console.log(r.data);
           return items.map((el) => ({
             name: el.name,
@@ -29,7 +32,8 @@ export class PageController {
   }
 
   @Get('test')
-  getItemsPerPageStubs() {
+  async getItemsPerPageStubs() {
+    await this.delay(1000);
     return pageListStub;
   }
 }
